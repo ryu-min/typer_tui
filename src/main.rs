982035ceda65a8e8f7c5d2b03dbb06002563b1a3
Ryumin Sergey <90983639+ryu-min@users.noreply.github.com>
2024-01-06 01:52:11 +0300

    add basic key press handling

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,10 @@ fn main() -> io::Result<()> {
     let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
 
     let mut should_quit = false;
+    let mut input_text = String::new(); // добавляем поле для хранения текста
     while !should_quit {
-        terminal.draw(ui)?;
-        should_quit = handle_events()?;
+        terminal.draw(|frame| ui(frame, &input_text))?; // передаем поле с текстом в функцию ui
+        should_quit = handle_events(&mut input_text)?; // передаем поле с текстом в функцию handle_events
     }
 
     disable_raw_mode()?;
@@ -22,20 +23,33 @@ fn main() -> io::Result<()> {
     Ok(())
 }
 
-fn handle_events() -> io::Result<bool> {
+fn handle_events(input_text: &mut String) -> io::Result<bool> {
     if event::poll(std::time::Duration::from_millis(50))? {
         if let Event::Key(key) = event::read()? {
             if key.kind == event::KeyEventKind::Press && key.code == KeyCode::Char('q') {
                 return Ok(true);
+            } else if key.kind == event::KeyEventKind::Release {
+                match key.code {
+                    KeyCode::Char(c) => {
+                        if c == '\u{1B}' { 
+                            input_text.clear(); 
+                        } else {
+                            input_text.push(c);
+                        }
+                    }
+                    _ => {
+                        return Ok(false);
+                    }
+                }
             }
        }
     }
     Ok(false)
 }
 
-fn ui(frame: &mut Frame) {
+fn ui(frame: &mut Frame, input_text: &str) { // принимаем поле с текстом как аргумент
     frame.render_widget(
-        Paragraph::new("Hello World!")
+        Paragraph::new(input_text) // используем поле с текстом для отображения
             .block(Block::default().title("Greeting").borders(Borders::ALL)),
         frame.size(),
     );
