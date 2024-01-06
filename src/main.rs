89b69f9ac77825d4443f4a6e73deb93b34d03d95
Ryumin Sergey <90983639+ryu-min@users.noreply.github.com>
2024-01-06 15:42:20 +0300

    start using tui_textarea

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use crossterm::{
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen}
 };
 use ratatui::{prelude::*, widgets::*};
+use tui_textarea::{Input, Key, TextArea};
 
 fn main() -> io::Result<()> {
     enable_raw_mode()?;
@@ -12,10 +13,15 @@ fn main() -> io::Result<()> {
     let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
 
     let mut should_quit = false;
-    let mut input_text = String::new(); 
+    let mut input_text = String::new();
+
+    let mut textarea = TextArea::default();
+    textarea.set_cursor_line_style(Style::default());
+    textarea.set_placeholder_text("Enter a valid float (e.g. 1.56)");
+
     while !should_quit {
-        terminal.draw(|frame| ui(frame, &input_text))?; 
-        should_quit = handle_events(&mut input_text)?; 
+        terminal.draw(|frame| ui(frame, &mut textarea))?;
+        should_quit = handle_events(&mut textarea)?;
     }
 
     disable_raw_mode()?;
@@ -23,33 +29,36 @@ fn main() -> io::Result<()> {
     Ok(())
 }
 
-fn handle_events(input_text: &mut String) -> io::Result<bool> {
-    if event::poll(std::time::Duration::from_millis(50))? {
-        if let Event::Key(key) = event::read()? {
-            if key.kind == event::KeyEventKind::Press && key.code == KeyCode::Char('q') {
-                return Ok(true)
-            } else if key.kind == event::KeyEventKind::Release {
-                match key.code {
-                    KeyCode::Char(c) => {
-                        input_text.push(c);
-                    }
-                    KeyCode::Backspace => {
-                        input_text.pop();
-                    }
-                    _ => {
-                        return Ok(false);
-                    }
-                }
-            }
-       }
+fn handle_events(text_area: &mut TextArea) -> io::Result<bool> {
+    // if event::poll(std::time::Duration::from_millis(50))? {
+    //     if let Event::Key(key) = event::read()? {
+    //         if key.kind == event::KeyEventKind::Press && key.code == KeyCode::Char('q') {
+    //             return Ok(true)
+    //         } else if key.kind == event::KeyEventKind::Release {
+    //             match key.code {
+    //                 KeyCode::Char(c) => {
+    //                     text_area.input(key);
+    //                 }
+    //                 _ => {
+    //                     return Ok(false);
+    //                 }
+    //             }
+    //         }
+    //    }
+    // }
+
+    match crossterm::event::read()?.into() {
+        Input { key: Key::Esc, .. } => return Ok(true),
+        input => {
+            text_area.input(input);
+        }
     }
     Ok(false)
 }
 
-fn ui(frame: &mut Frame, input_text: &str) { // принимаем поле с текстом как аргумент
+fn ui(frame: &mut Frame, text_area: &mut TextArea) { // принимаем поле с текстом как аргумент
     frame.render_widget(
-        Paragraph::new(input_text) // используем поле с текстом для отображения
-            .block(Block::default().title("Greeting").borders(Borders::ALL)),
+        text_area.widget(),
         frame.size(),
     );
 }
